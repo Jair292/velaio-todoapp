68c16d80bb128c06058d1fb0d3da24a7bd01c279
Jair292 <jair.pinto1992@gmail.com>
2024-10-21 13:39:15 -0500

    -Updated angular version to 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [],
 })
 export class AppComponent {}
